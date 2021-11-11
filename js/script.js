0096bb4a6662eb2567ec418895ac041cd318e65d
Davide Tavella <tavella01@gmail.com>
2021-11-11 15:48:52 +0100

    tentativo attivazione thumbs

--- js/script.js
+++ js/script.js
@@ -4,9 +4,14 @@ const app = new Vue({
 
     data:{
         images:['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'],
-        
+        // thumbs:[],
         // inizializzo il counter con cui attivo le foto
-        counter: 0
+        counter: 0,
+        isActive1: false,
+        isActive2: false,
+        isActive3: false,
+        isActive4: false,
+        isActive5: false,
     },
 
     mounted(){
@@ -14,13 +19,15 @@ const app = new Vue({
         // azione per far partire automaticamente la funzione "nextSlide ogni 1,5 secondi" 
         setInterval(() => {
             this.nextSlide();
-        }, 1500)
+        }, 15000)
+        
     },
 
     methods:{
 
         nextSlide(){
             this.counter++;
+            console.log(this.counter);
             if(this.counter > this.images.length -1){
                 this.counter = 0;
             }
@@ -31,12 +38,19 @@ const app = new Vue({
             if(this.counter < 0){
                 this.counter = this.images.length -1;
             }
+        },
+
+        activeThumb(){
+
+            if(counter = 1){
+                isActive1 = true;
+                
+            }
         }
-
     }
-
-    
 
 
 
 })
+
+// funzione per aggiungere bordo bianco alla foto
