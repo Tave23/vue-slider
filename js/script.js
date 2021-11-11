49c6e1099efa0a3143b8134d5a9587fccff13083
Davide Tavella <tavella01@gmail.com>
2021-11-11 17:40:23 +0100

    aggiunta testo su foto

--- js/script.js
+++ js/script.js
@@ -3,15 +3,41 @@ const app = new Vue({
     el: '#app',
 
     data:{
-        images:['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'],
+    
+        images:[
+            {
+                url:'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit',
+            },
+            {
+                url:'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum, dolor'
+            },
+            {
+                url:'img/03.jpg',
+                title: 'Gran Bretagna',
+                text:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                url:'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum,'
+            },
+            {
+                url:'img/05.jpg',
+                title: 'Paradise',
+                text: 'magnam dolores dolorum corporis.'
+            },
+    ],
+
         // thumbs:[],
         // inizializzo il counter con cui attivo le foto
         counter: 0,
-        isActive1: false,
-        isActive2: false,
-        isActive3: false,
-        isActive4: false,
-        isActive5: false,
+        indice: 0,
+
+        isActive: false
     },
 
     mounted(){
@@ -20,37 +46,60 @@ const app = new Vue({
         setInterval(() => {
             this.nextSlide();
         }, 15000)
-        
+
     },
 
     methods:{
 
         nextSlide(){
             this.counter++;
+            this.indice++;
+
             console.log(this.counter);
+            console.log(this.indice);
+
+            // this.isActive = true;
+
             if(this.counter > this.images.length -1){
                 this.counter = 0;
+            }
+
+            if(this.indice > this.images.length -1){
+                this.indice = 0;
             }
         },
 
         prevSlide(){
             this.counter--;
+            this.indice--;
+
+            console.log(this.counter);
+            console.log(this.indice);
+
+            // this.isActive = true;
+
             if(this.counter < 0){
                 this.counter = this.images.length -1;
             }
+
+            if(this.indice < 0){
+                this.indice = this.images.length -1;
+            }
+
         },
 
-        activeThumb(){
+        checkActive(){
+            if(this.indice == this.counter){
+           
 
-            if(counter = 1){
-                isActive1 = true;
-                
             }
+            
         }
+        
     }
-
-
 
 })
 
 // funzione per aggiungere bordo bianco alla foto
+
+    
